d6da6a51545694da15fdcde57e34319266e6e3b3
DoTheyKnow22 <140292076+Theydontknow22@users.noreply.github.com>
2025-03-09 17:39:12 -0700

    Brent/day2 (#8)
    * adding git ignore
    
    * homepage update
    
    * html updates
    
    * flask application
    
    * flask
    
    * rendered_templates
    
    ---------
    
    Co-authored-by: B <brent@Dotheyknow2>

---
+++ app/site.py
@@ -0,0 +1,41 @@
+from flask import Flask, jsonify, render_template
+import psycopg2
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env
+load_dotenv()
+
+app = Flask(__name__, template_folder='/var/www/myapp')
+
+# Get PostgreSQL credentials from the .env file
+db_host = os.getenv('DB_HOST')
+db_name = os.getenv('DB_NAME')
+db_user = os.getenv('DB_USER')
+db_password = os.getenv('DB_PASSWORD')
+
+# Connect to PostgreSQL
+def connect_to_db():
+    return psycopg2.connect(
+        host=db_host,
+        database=db_name,
+        user=db_user,
+        password=db_password
+    )
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/data')
+def get_data():
+    conn = connect_to_db()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users LIMIT 5;")
+    rows = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return jsonify(rows)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
