731c2246b21847ec3c4c7423927beeef46b6caf4
B <brent@Dotheyknow2.>
2025-03-15 10:03:39 -0700

    remove

--- site.py
+++ site.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, session
 from flask_login import LoginManager
 import psycopg2
 from dotenv import load_dotenv
